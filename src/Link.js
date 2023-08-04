829efe5a08fae157229f3173eaf624d733a6bc70
neanix <nav.hamedi@gmail.com>
2023-08-04 23:34:18 +0200

    spreading components and renaming folders and files

---
+++ src/Link.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header.js';
+import Body from './components/Body.js';
+import Footer from './components/Footer.js';
+
+function Link() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default Link;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Hello I'm header!</h1>
+        </div>
+    );
+}
+
+export default Header;
