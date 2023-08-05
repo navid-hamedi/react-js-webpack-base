9c95ba3ab6498c04d86d301f10a2b804f9a8c28d
neanix <nav.hamedi@gmail.com>
2023-08-05 19:09:29 +0200

    updates and upgrades to sass

--- src/Link.js
+++ src/Link.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 import Footer from './components/Footer.js';
+import './styles/main.scss';
 
 function Link() {
   return (
-    <div>
+    <div className='container'>
       <Header />
       <Body />
       <Footer />
